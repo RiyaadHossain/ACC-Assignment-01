425561cf217222261441aa42689477431e5bb20e
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-03 14:47:09 +0600

    new api added

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get("/", (req, res) => res.send({ message: "Hello World!" }))
+app.get("/home", (req, res) => res.send({ message: "This is home, bro! Stay here" }))
 app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
