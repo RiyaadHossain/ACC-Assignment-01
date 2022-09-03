041eb7be19fc994e4fabf7d6a18304fa2cd61664
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-03 14:51:05 +0600

    assignment code added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000
+const userRoute = require("./router/userRoute")
+
 const app = express()
 
-app.get("/", (req, res) => res.send({ message: "Hello World!" }))
-app.get("/home", (req, res) => res.send({ message: "This is home, bro! Stay here" }))
-app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
+// Middleware
+app.use(express.json())
+app.use("/user", userRoute)
+
+// Health Check
+app.get("/", (req, res) => {
+    res.send("Hello, Programming Hero!")
+})
+
+// Listen to PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`)
+})
